2abfc655bfafa44b60830c284ea3c2ea5fc92891
shadowplayzman <callofdutystars1234@gmail.com>
2026-08-31 20:07:25 +0530

    small chnages

--- physics-engine/main.cpp
+++ physics-engine/main.cpp
@@ -43,7 +43,6 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	// create window
 	GLFWwindow* window = glfwCreateWindow(width, height, "Physics Engine", NULL, NULL);
